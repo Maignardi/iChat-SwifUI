e44542997e9117c7e325d4e75a4089903035bca8
mmaignardi <matheus.maignardi@meliuz.com.br>
2022-05-07 16:18:52 -0400

    Initial Commit

---
+++ iChat-SwifUI/iChat_SwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChat_SwifUIApp.swift
+//  iChat-SwifUI
+//
+//  Created by Matheus Mendonça Maignardi on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct iChat_SwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
